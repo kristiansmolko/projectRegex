1f7200559fc9fd365fedf093800fd02f0fbe1804
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-01-19 12:39:19 +0100

    Added isValidOP

--- NoRegex.java
+++ NoRegex.java
@@ -1,12 +1,12 @@
 public class NoRegex {
-    public boolean isValidSPZ(String text){
-        if (text == null || text.length() != 7)
+    public boolean isValidOP(String text){
+        if (text == null || text.length() != 8)
             return false;
         text=text.toUpperCase().trim();
         for (int i = 0; i <= 1; i++)
             if (!Character.isLetter(text.charAt(i)))
                 return false;
-        for (int i = 2; i <= 6; i++)
+        for (int i = 2; i <= 7; i++)
             if (!Character.isLetterOrDigit(text.charAt(i)))
                 return false;
         return true;
