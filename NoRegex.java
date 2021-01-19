9a02267b4a7c4e763fb0613ca09ced0564e8bf45
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-01-19 12:28:35 +0100

    Added isValidSPZ

---
+++ NoRegex.java
@@ -0,0 +1,14 @@
+public class NoRegex {
+    public boolean isValidSPZ(String text){
+        if (text == null || text.length() != 7)
+            return false;
+        text=text.toUpperCase().trim();
+        for (int i = 0; i <= 1; i++)
+            if (!Character.isLetter(text.charAt(i)))
+                return false;
+        for (int i = 2; i <= 6; i++)
+            if (!Character.isLetterOrDigit(text.charAt(i)))
+                return false;
+        return true;
+    }
+}
