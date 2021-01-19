b1ed40d287103766210eaca0ae29f69a737bfc3d
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-01-19 13:29:09 +0100

    Some changes

--- NoRegex.java
+++ NoRegex.java
@@ -2,7 +2,7 @@ public class NoRegex {
     public boolean isValidOP(String text){
         if (text == null || text.length() != 8)
             return false;
-        text=text.toUpperCase().trim();
+        text=text.toUpperCase();
         for (int i = 0; i <= 1; i++)
             if (!Character.isLetter(text.charAt(i)))
                 return false;
@@ -10,6 +10,7 @@ public class NoRegex {
             if (!Character.isLetterOrDigit(text.charAt(i)))
                 return false;
         return true;
+        //something here
     }
 
     public boolean isValidSPZ(String text) {
