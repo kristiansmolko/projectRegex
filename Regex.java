c99315393ec1f2f47b58c654c6772d903e207628
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-01-21 10:39:48 +0100

    Added regex class

---
+++ Regex.java
@@ -0,0 +1,13 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+
+    public boolean checkValue(String text){
+        if (text == null)
+            return false;
+        Pattern pattern = Pattern.compile("^(([1-9]\\d+|\\d)[\\.,]([1-9]\\d|\\d[1-9])|([1-9]\\d+))$|^[0]$");
+        Matcher matcher = pattern.matcher(text);
+        return matcher.find();
+    }
+}
